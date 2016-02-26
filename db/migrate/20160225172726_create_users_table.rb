2a4b34fb15af724734191cb5d397daee94abbd3c
SteveSnow <Steven.Snow@outlook.com>
2016-02-26 12:01:11 -0500

    added git ignore

--- db/migrate/20160225172726_create_users_table.rb
+++ db/migrate/20160225172726_create_users_table.rb
@@ -1,4 +1,12 @@
 class CreateUsersTable < ActiveRecord::Migration
   def change
+  		create_table :users do |u|
+  		u.string :fname
+  		u.string :lname
+  		u.string :email
+  		u.string :password
+  		u.string :follows_id
+  		u.datetime :created_at,default: Time.now
+  	end
   end
 end
