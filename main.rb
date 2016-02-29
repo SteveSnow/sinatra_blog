8ee7a92cf61ab922441f5757c71fc21166a7ae90
Nancy Blayney <nblayney@g.cofc.edu>
2016-02-28 20:28:42 -0500

    added Profile branch with profile page and settings page to edit user account info

--- main.rb
+++ main.rb
@@ -11,3 +11,50 @@ get "/" do
 	@user=User.find(1)
 	erb:home
 end
+
+
+get'/profile/:id' do
+	@user = User.find(params[:id])
+	@posts = @user.posts
+	if session[:user_id]
+		@me = current_user
+		erb :profile 
+	else
+		erb :profile
+	end
+end
+
+get '/settings/:id' do
+	@user = User.find(params[:id])
+	erb :settings
+end
+
+post '/settings' do
+		puts "*********************************************************************************"
+	@user = User.find(session[:user_id])
+	@user.fname = params[:fname]
+	@user.lname = params[:lname]
+	@user.email = params[:email]
+	@user.password = params[:password]
+	@user.save
+	redirect '/'
+end
+
+post '/delete-profile' do
+
+	@user = current_user
+
+	if params[:password].empty?
+		flash[:alert] = "Password cannot be empty!"
+		redirect back
+	elsif @user.password == params[:password]
+		User.destroy(@user)
+		session.clear
+		flash[:notice] = "Your Twizzle account been deleted!"
+		redirect '/'
+	else
+		flash[:alert] = "Password was incorrect!"
+		redirect back
+	end
+
+end
